b79b7e4fd783110d25658079f5f1c714043b6154
Elshan <elshanmal@code.edu.az>
2021-12-05 17:36:58 +0400

    Homework

---
+++ Introduction/Introduction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 16;
+            int b = 47;
+
+           // Console.WriteLine(a);
+           // Console.WriteLine(b);
+
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+
+
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
